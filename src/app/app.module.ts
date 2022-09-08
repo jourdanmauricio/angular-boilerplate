26112b2ae490880c878cab9b6e071071c1a67f75
Mauricio <jourdanmauricio@gmail.com>
2022-09-08 14:53:40 -0300

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Modules
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy,
+    },
+  ],
+
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
