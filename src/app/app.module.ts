77ef7a2e3bd3409fe03ea5eb46cef071c05486cb
Mauricio <jourdanmauricio@gmail.com>
2022-09-08 15:10:07 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,12 @@ import { AppComponent } from './app.component';
 // Modules
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SkeletonComponent, FooterComponent, NavigationComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
   providers: [
     {
